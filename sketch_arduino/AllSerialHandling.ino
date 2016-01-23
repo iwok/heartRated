52fb469a1e21b0d12ae951c414dd4bb5db4da440
iwok <iwo@posteo.de>
2016-01-23 15:50:09 +0100

    removed skin output to serial

--- sketch_arduino/AllSerialHandling.ino
+++ sketch_arduino/AllSerialHandling.ino
@@ -9,7 +9,6 @@ void serialOutput(){   // Decide How To Output Serial.
 //  Decides How To OutPut BPM and SKIN Data
 void serialOutputWhenBeatHappens(){    
         sendDataToSerial('B',BPM);   // send heart rate with a 'B' prefix
-        //sendDataToSerial('S',SKIN);   // send time between beats with a 'Q' prefix
 }
 
 //  Sends Data to Pulse Sensor Processing App, Native Mac App, or Third-party Serial Readers. 
