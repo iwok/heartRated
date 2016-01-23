abaf8a7360c4ad912bdf291d515f6aa493c0cd68
iwok <iwo@posteo.de>
2016-01-23 15:53:44 +0100

    clean up / naming

--- sketch_arduino/sketch_arduino.ino
+++ sketch_arduino/sketch_arduino.ino
@@ -1,14 +1,12 @@
 //////////
-/////////  moviEmotions 
+/////////  heartRated 
 /////////
 
 //  Variables
-int pulsePin = A0; // Pulse Sensor purple wire connected to analog pin 0
-int skinPin  = A1; // Skin Sensor 
+int pulsePin = A5; // Pulse Sensor purple wire connected to analog pin 0
 
 // Volatile Variables, used in the interrupt service routine!
 volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
-volatile int SKIN;
 volatile int Signal;                // holds the incoming raw data
 volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded! 
 volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat". 
@@ -25,7 +23,6 @@ void setup() {
   
   interruptSetup(); // sets up to read Pulse Sensor signal every 2mS 
   Serial.println("Waiting for Values ...");
-}
 
     // set the data rate for the Serial port
   Serial.begin(9600);
@@ -38,8 +35,9 @@ void setup() {
   softSerial.println("Software Serial start ...");
 }
 
+void loop() { 
   //serialOutput() ;  
-  getMittelwertSkin();
+  //getMittelwertSkin();
   
   if (QS == true){     // A Heartbeat Was Found
                        // BPM and IBI have been Determined
